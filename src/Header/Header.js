74084a5aa76488cca153dad3cd44f94d665d2a2b
Matt Umland <mattumland@gmail.com>
2021-06-29 11:56:13 -0400

    Add nav bar with buttons

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -4,7 +4,23 @@ import './Header.scss';
 const header = () => {
     return (
         <header>
-
+            <nav>
+                <li>
+                    <button>MATT UMLAND</button>
+                </li>
+                <li>
+                    <button>CODES</button>
+                </li>
+                <li>
+                    <button>EXHIBITS</button>
+                </li>
+                <li>
+                    <button>GAMES</button>
+                </li>
+                <li>
+                    <button>MUSIC</button>
+                </li>
+            </nav>
         </header>
     )
 }
