f46ba9d9758d02ffa4abb15577efc66919016bb5
Matt Umland <mattumland@gmail.com>
2021-06-30 17:44:33 -0400

    Add structure for bio header

--- src/App/App.js
+++ src/App/App.js
@@ -1,7 +1,15 @@
 import Header from '../Header/Header'
 import './App.scss';
 import { useState } from 'react';
-import portrait from '../images/temp-portrait.jpg'
+import portrait from '../images/temp-portrait.png'
+import gitlogo from '../images/gitlogo.png';
+import gramlogo from '../images/gramlogo.png';
+import itchlogo from '../images/itchlogo.png';
+import camplogo from '../images/camplogo.png';
+import twitlogo from '../images/twitlogo.png';
+
+
+
 
 const App = () => {
 
@@ -19,11 +27,19 @@ const App = () => {
       />
       <article className="home-view">
         <div className="portrait-container">
-          <img className="portait-img" src={[portrait]}/>
+          <img className="portrait-img" src={portrait} alt="portrait of matt umland"/>
         </div>
         <section className="bio-container">
-          <h1 className='name'>MATT UMLAND</h1>
-
+          <div className="bio-header">
+            <h1 className='name'>MATT UMLAND</h1>
+            <ul className='icon-links'>
+              <li>
+                <a href="https://github.com/mattumland" target="_blank" rel="noopener noreferrer">
+                  <img src={gitlogo} alt="the Github logo"/>
+                </a>
+              </li>
+            </ul>
+          </div>
         </section>
       </article>
  
