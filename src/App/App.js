862cece76015372a71e32996accd0268b85bb710
Matt Umland <mattumland@gmail.com>
2021-06-30 13:46:23 -0400

    Add state for tracking scroll location 'page'

--- src/App/App.js
+++ src/App/App.js
@@ -1,10 +1,21 @@
 import Header from '../Header/Header'
 import './App.scss';
+import { useState } from 'react';
 
 const App = () => {
+
+  const [page, setPage] = useState('home')
+
+  const updatePage = (newPage) => {
+    setPage(newPage);
+  }
+
   return (
     <div className="app-background">
-      <Header />
+      <Header 
+        page={page}
+        updatePage={updatePage}
+      />
     </div>
   );
 }
